e8503454c28d027948b2e5286aa780893e613e60
Yangjie Qi <qiyangjie419@gmail.com>
2021-02-05 13:50:08 -0500

    Update problem_6.py

--- project2/problem_6.py
+++ project2/problem_6.py
@@ -11,14 +11,6 @@ class LinkedList:
     def __init__(self):
         self.head = None
 
-    def __str__(self):
-        cur_head = self.head
-        out_string = ""
-        while cur_head:
-            out_string += str(cur_head.value) + " -> "
-            cur_head = cur_head.next
-        return out_string
-
     def append(self, value):
 
         if self.head is None:
@@ -37,18 +29,68 @@ class LinkedList:
         while node:
             size += 1
             node = node.next
-
         return size
+
+    def __str__(self):
+        cur_head = self.head
+        out_string = ""
+        while cur_head:
+            out_string += str(cur_head.value) + " -> "
+            cur_head = cur_head.next
+        return out_string
 
 
 def union(llist_1, llist_2):
     # Your Solution Here
-    pass
+    # Make the first list as the ordered list
+
+    # Read the value of llist to a python list.
+    union_list = []
+    current_node = llist_1.head
+    while current_node:
+        union_list.append(current_node.value)
+        current_node = current_node.next
+
+    current_node = llist_2.head
+    while current_node:
+        union_list.append(current_node.value)
+        current_node = current_node.next
+
+    # remove the duplications
+    union_list = list(set(union_list))
+
+    # Configure the output as linked list
+    output_list = LinkedList()
+    for value in union_list:
+        output_list.append(value)
+
+    return output_list
 
 
 def intersection(llist_1, llist_2):
-    # Your Solution Here
-    pass
+    list_1 = []
+    list_2 = []
+
+    current_node = llist_1.head
+    while current_node:
+        list_1.append(current_node.value)
+        current_node = current_node.next
+
+    current_node = llist_2.head
+    while current_node:
+        list_2.append(current_node.value)
+        current_node = current_node.next
+
+    list_1 = set(list_1)
+    list_2 = set(list_2)
+
+    list_3 = [value for value in list_1 if value in list_2]
+
+    # Configure the output as linked list
+    output_list = LinkedList()
+    for value in list_3:
+        output_list.append(value)
+    return output_list
 
 
 if __name__ == '__main__':
