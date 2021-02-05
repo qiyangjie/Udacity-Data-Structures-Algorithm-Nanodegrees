97cd16c8fcd9c79a581ee6cb3305d5ef49985ef8
Yangjie Qi <qiyangjie419@gmail.com>
2021-02-05 11:45:44 -0500

    Finished problem_5.py and problem_6.py

---
+++ project2/problem_5.py
@@ -0,0 +1,54 @@
+import hashlib
+import datetime
+
+
+def calc_hash():
+    sha = hashlib.sha256()
+
+    hash_str = "We are going to encode this string of data!".encode('utf-8')
+
+    sha.update(hash_str)
+
+    return sha.hexdigest()
+
+
+class Block:
+    def __init__(self, data, previous_hash):
+        self.timestamp = datetime.datetime.now()
+        self.data = data
+        self.previous_hash = previous_hash
+        self.hash = calc_hash()
+        self.next = None
+
+    def __repr__(self):
+        return "time:{}\n" \
+               "data:{}\n" \
+               "hash:{}\nn" \
+               "previous_hash: {}\n".format(self.timestamp, self.data, self.hash, self.previous_hash)
+
+
+class BlockChain:
+    def __init__(self):
+        self.head = None
+
+    def append(self, value):
+        if self.head is None:
+            self.head = Block(value, 0)
+            return
+
+        node = self.head
+        while node.next:
+            node = node.next
+
+        node.next = Block(value, node.hash)
+
+
+if __name__ == '__main__':
+    myChain = BlockChain()
+    myChain.append("information 1")
+    myChain.append("information 2")
+    myChain.append("information 3")
+
+    print(myChain.head)
+    print(myChain.head.next)
+    print(myChain.head.next.next)
