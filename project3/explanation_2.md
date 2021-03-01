da2751c0ce592afee8afc19327c0a88ed91343c7
Yangjie Qi <qiyangjie419@gmail.com>
2021-03-01 16:32:59 -0500

    Finished explanation_2.md and rotated_array_search.py

--- project3/explanation_2.md
+++ project3/explanation_2.md
@@ -1,11 +1,3 @@
-You are given a sorted array which is rotated at some random pivot point.
-
-Example: [0,1,2,4,5,6,7] might become [4,5,6,7,0,1,2]
-
-You are given a target value to search. If found in the array return its index, otherwise return -1.
-
-You can assume there are no duplicates in the array and your algorithm's runtime complexity must be in the order of O(log n).
-
-Example:
-
-Input: nums = [4,5,6,7,0,1,2], target = 0, Output: 4
+- Utilize Binary Search
+- Time complexity is O(log(n))
+- Space complexity is O(1)
--- project3/rotated_array_search.py
+++ project3/rotated_array_search.py
@@ -1,4 +1,91 @@
+def ordered_array_search_recursive(input_list, number, begin_index, end_index):
 
+    if input_list[begin_index] == number:
+        return begin_index
+
+    if end_index - begin_index <= 0:
+        return -1
+
+    current_index = (begin_index + end_index + 1) // 2
+    mid = input_list[current_index]
+    if mid == number:
+        return current_index
+    elif mid > number:
+        return ordered_array_search_recursive(input_list, number, begin_index, current_index)
+    else:
+        return ordered_array_search_recursive(input_list, number, current_index, end_index)
+
+
+# def rotated_array_search_recursive(input_list, number, begin_index, end_index):
+#     current_index = (begin_index + end_index) // 2
+#
+#     begin = input_list[begin_index]
+#     mid = input_list[current_index]
+#     end = input_list[end_index]
+#
+#     if mid > begin and mid > end:
+#         # left is sorted and right is unsorted
+#         if number == mid:
+#             return current_index
+#         elif mid < number:
+#             return rotated_array_search_recursive(input_list, number, current_index, end_index)
+#         elif mid > number >= begin:
+#             return ordered_array_search_recursive(input_list, number, begin_index, current_index)
+#         elif mid > number and number < begin:
+#             return rotated_array_search_recursive(input_list, number, current_index, end_index)
+#     elif begin < mid < end:
+#         # The list is sorted
+#         if number == mid:
+#             return current_index
+#         elif mid < number:
+#             return ordered_array_search_recursive(input_list, number, current_index, end_index)
+#         elif mid > number:
+#             return ordered_array_search_recursive(input_list, number, begin_index, current_index)
+#     elif begin > mid:
+#         # left is unsorted and right is sorted
+#         if number == mid:
+#             return current_index
+#         elif mid < number:
+#             if number < end:
+#                 return ordered_array_search_recursive(input_list, number, current_index, end_index)
+#             else:
+#                 return rotated_array_search_recursive(input_list, number, begin_index, current_index)
+#         elif mid > number:
+#             return rotated_array_search_recursive(input_list, number, begin_index, current_index)
+#     else:
+#         return -1
+
+
+def rotated_array_search_recursive(input_list, number, begin_index, end_index):
+    current_index = (begin_index + end_index) // 2
+
+    begin = input_list[begin_index]
+    mid = input_list[current_index]
+    end = input_list[end_index]
+
+    if mid > begin and mid > end:
+        # left is sorted and right is unsorted
+        if number == mid:
+            return current_index
+        elif mid < number:
+            return rotated_array_search_recursive(input_list, number, current_index, end_index)
+        elif mid > number >= begin:
+            return ordered_array_search_recursive(input_list, number, begin_index, current_index)
+        elif mid > number and number < begin:
+            return rotated_array_search_recursive(input_list, number, current_index, end_index)
+    elif begin > mid:
+        # left is unsorted and right is sorted
+        if number == mid:
+            return current_index
+        elif mid < number:
+            if number < end:
+                return ordered_array_search_recursive(input_list, number, current_index, end_index)
+            else:
+                return rotated_array_search_recursive(input_list, number, begin_index, current_index)
+        elif mid > number:
+            return rotated_array_search_recursive(input_list, number, begin_index, current_index)
+    else:
+        return -1
 
 
 def rotated_array_search(input_list, number):
@@ -6,11 +93,12 @@ def rotated_array_search(input_list, number):
     Find the index by searching in a rotated sorted array
 
     Args:
+       number:
        input_list(array), number(int): Input array to search and the target
     Returns:
        int: Index or -1
     """
-    return -1
+    return rotated_array_search_recursive(input_list, number, 0, len(input_list)-1)
 
 
 def linear_search(input_list, number):
